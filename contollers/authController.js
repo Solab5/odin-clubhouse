f686fd3a5f96cdd9b61353e8ce098dc559db4206
morris <morristwinomugisha2000@gmail.com>
2025-02-01 10:16:57 +0300

    fix: add user to database

--- contollers/authController.js
+++ contollers/authController.js
@@ -1,6 +1,29 @@
+const db = require('../db/queries')
+const bcrypt = require('bcryptjs');
+
 exports.getSignup = (req, res) => {
     res.render('signup', { title: "sign up"});
 }
 
-exports.postSignup = (req, res) => {
-}
+exports.postSignup = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, confirmPassword } = req.body;
+        if (password !== confirmPassword) {
+            return res.status(400).render('signup', {
+                title: "sign up",
+                error: "Passwords donot match"
+                
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db.createUser(firstName, lastName, email, hashedPassword);
+        res.redirect('/login');
+    } catch (error) {
+        console.log('Signup error:', error);
+        res.render('signup', {
+            title: "sign up",
+            error: "Error creating account"
+        });
+    }
+};
--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,14 @@
 const pool = require('./pool');
 
 exports.createUser = async (firstName, lastName, email, password) => {
-    await pool.query(
+    try {
+        await pool.query(
         "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
         [firstName, lastName, email, password]
-    )
+        );
+    } catch(error) {
+        console.log("failed to add user to data base with Error:", error)
+        throw error;
+    }
+    
 }
