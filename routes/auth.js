f6c979880e519e030d1d68d45e6937ebb07b40c5
morris <morristwinomugisha2000@gmail.com>
2025-02-02 11:10:47 +0300

    fix: rename controller name

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const authController = require('../contollers/authController')
+const authController = require('../controllers/authController')
 const authRouter = Router();
 
 authRouter.get('/signup', authController.getSignup);
